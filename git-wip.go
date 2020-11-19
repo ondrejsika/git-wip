13af17f57f7b0406a7a6a47cabeeef67f4945b5d
Ondrej Sika <ondrej@ondrejsika.com>
2020-11-19 19:01:11 +0100

    Add git-wip.go in first version (v1.0.0)

---
+++ git-wip.go
@@ -0,0 +1,57 @@
+// git-wip
+//
+// Author: Ondrej Sika <ondrej@ondrejsika.com>
+// Source: https://github.com/ondrejsika/git-wip
+//
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/user"
+	"path/filepath"
+	"strconv"
+
+	"github.com/go-git/go-git/v5"
+)
+
+type stateStruct map[string]int
+
+func main() {
+	// Get stateFilePath ~/.gitwip.state.json
+	usr, _ := user.Current()
+	stateFilePath := filepath.Join(usr.HomeDir, ".git-wip.state.json")
+
+	// Setup state
+	state := stateStruct{}
+
+	// Load state if state file exists
+	_, err := os.Stat(stateFilePath)
+	if !os.IsNotExist(err) {
+		jsonFile, _ := os.Open(stateFilePath)
+		byteValue, _ := ioutil.ReadAll(jsonFile)
+		json.Unmarshal(byteValue, &state)
+	}
+
+	// Get latest WIP count from state and increase by 1
+	repoPath, _ := filepath.Abs(".")
+	if _, ok := state[repoPath]; !ok {
+		state[repoPath] = 0
+	}
+	state[repoPath] = state[repoPath] + 1
+
+	// Commit all changes
+	r, _ := git.PlainOpen(".")
+	w, _ := r.Worktree()
+	w.Add(".")
+	commit, _ := w.Commit("WIP "+strconv.Itoa(state[repoPath])+"\n\nCommited by https://github.com/ondrejsika/git-wip", &git.CommitOptions{})
+	obj, _ := r.CommitObject(commit)
+	fmt.Println(obj)
+
+	// Save new state
+	file, _ := json.Marshal(state)
+	ioutil.WriteFile(stateFilePath, file, 0644)
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/ondrejsika/git-wip
+
+go 1.15
+
+require (
+	github.com/go-git/go-git v4.7.0+incompatible
+	github.com/go-git/go-git/v5 v5.2.0
+	gopkg.in/src-d/go-git.v4 v4.13.1 // indirect
+)
